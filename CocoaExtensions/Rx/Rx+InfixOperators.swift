ed2cb8d6a0baa0d0dc2e443a6e0bb2df5ee1d57a
Abdurahim Jauzee <jauzee@mail.ru>
2017-11-12 17:29:18 +0300

    Update Rx infix functions

--- CocoaExtensions/Rx/Rx+InfixOperators.swift
+++ CocoaExtensions/Rx/Rx+InfixOperators.swift
@@ -9,6 +9,7 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+// MARK: - Precedence Groups
 precedencegroup Binding {
   associativity: none
   higherThan: Additive
@@ -19,24 +20,63 @@ precedencegroup Additive {
   higherThan: MultiplicationPrecedence
 }
 
-infix operator >>> : Additive
-
-public func >>> (lhs: Disposable, rhs: DisposeBag) {
-  rhs.insert(lhs)
+precedencegroup Right {
+  associativity: right
+  higherThan: Additive
 }
 
+// MARK: - Dispose bag
+infix operator >>> : Additive
+
+public func >>> (lhs: Disposable?, rhs: DisposeBag) {
+  if let disposable = lhs {
+    rhs.insert(disposable)
+  }
+}
+
+// MARK: - Two-way binding
 infix operator <-> : Binding
 
 public func <-> <T>(property: ControlProperty<T>, variable: Variable<T>) -> Disposable {
-  let bindToUIDisposable = variable.asObservable()
-    .bind(to: property)
+  let uiDisposable = variable.asObservable().bind(to: property)
   let bindToVariable = property
     .subscribe(onNext: { n in
       variable.value = n
     }, onCompleted:  {
-      bindToUIDisposable.dispose()
+      uiDisposable.dispose()
     })
   
-  return Disposables.create(bindToUIDisposable, bindToVariable)
+  return Disposables.create(uiDisposable, bindToVariable)
 }
 
+// MARK: - One-way binding
+infix operator <-- : Right
+
+public func <-- <T>(binder: Binder<T>, observable: Observable<T>) -> Disposable {
+  let disposable = observable.bind(to: binder)
+  return disposable
+}
+
+public func <-- <T>(binder: Binder<T>, variable: Variable<T>) -> Disposable {
+  let disposable = variable.asObservable().bind(to: binder)
+  return disposable
+}
+
+// MARK: - Convert to Optional
+prefix operator ?=
+
+public prefix func ?= <T>(variable: Variable<T>) -> Observable<T?> {
+  let optional = variable.asObservable()
+    .map { value -> T? in
+      return value
+    }
+  return optional
+}
+
+public prefix func ?= <T>(observable: Observable<T>) -> Observable<T?> {
+  let optional = observable
+    .map { value -> T? in
+      return value
+    }
+  return optional
+}
