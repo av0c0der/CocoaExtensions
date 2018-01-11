84041a4d0d729af6337ef44ffd7b31bc3e72faa8
Abdurahim Jauzee <jauzee@mail.ru>
2018-01-11 20:06:12 +0300

    Fix access level

--- CocoaExtensions/UI/UIKit/UIButton+TouchArea.swift
+++ CocoaExtensions/UI/UIKit/UIButton+TouchArea.swift
@@ -5,16 +5,20 @@
 //  Created by Abdurahim Jauzee on 11/01/2018.
 //
 
+#if os(OSX)
+  // Skip macOS
+#else
+
 import UIKit
 
 
 private var pTouchAreaEdgeInsets: UIEdgeInsets = .zero
 
-extension UIButton {
+public extension UIButton {
   
   /// Touchable area of the button.
   /// Default is `.zero`. Provide insets with negative values to expand the touchable area.
-  var touchAreaEdgeInsets: UIEdgeInsets {
+  public var touchAreaEdgeInsets: UIEdgeInsets {
     get {
       guard let value = objc_getAssociatedObject(self, &pTouchAreaEdgeInsets) as? NSValue else { return .zero }
       var edgeInsets: UIEdgeInsets = .zero
@@ -40,3 +44,5 @@ extension UIButton {
   }
   
 }
+
+#endif
