6d24ab529def1026b44a27140f9462f6006eac2b
Abdurahim Jauzee <a.jauzee@gmail.com>
2019-01-12 17:41:50 +0200

    Use UIEdgeInsets constructor

--- CocoaExtensions/UI/EdgeInsets.swift
+++ CocoaExtensions/UI/EdgeInsets.swift
@@ -17,17 +17,11 @@
 public extension EdgeInsets {
   
   public init(_ padding: CGFloat, sidePadding: CGFloat? = nil) {
-    top = padding
-    bottom = padding
-    left = sidePadding ?? padding
-    right = sidePadding ?? padding
+    self.init(top: padding, left: sidePadding ?? padding, bottom: padding, right: sidePadding ?? padding)
   }
   
   public init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil, otherSides: CGFloat? = nil) {
-    self.top = top ?? otherSides ?? 0
-    self.left = left ?? otherSides ?? 0
-    self.bottom = bottom ?? otherSides ?? 0
-    self.right = right ?? otherSides ?? 0
+    self.init(top: top ?? otherSides ?? 0, left: left ?? otherSides ?? 0, bottom: bottom ?? otherSides ?? 0, right: right ?? otherSides ?? 0)
   }
   
 }
